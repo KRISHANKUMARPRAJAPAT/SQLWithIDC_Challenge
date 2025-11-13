aea469e5b5f90bc533f37f4cc1d36155f478e3d6
KRISHAN KUMAR <122435688+KRISHANKUMARPRAJAPAT@users.noreply.github.com>
2025-11-13 11:31:04 +0530

    Add files via upload

---
+++ Day_2.sql
@@ -0,0 +1,8 @@
+/* Q.2 Find all patients admitted to 'Surgery' service with a satisfaction score below 70, 
+	showing their patient_id, name, age, and satisfaction score*/
+
+select 
+	patient_id, name, age, satisfaction
+from patients
+where service = 'surgery'
+	and satisfaction <70
